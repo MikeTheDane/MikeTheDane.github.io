727bddb94120dc68fa5e03bd8ae15047f14fac65
MikeTheDane <MikeTheDane@users.noreply.github.com>
2020-05-11 14:20:51 -0700

    Update w_button_obj.js

--- ePOC/w_button_obj.js
+++ ePOC/w_button_obj.js
@@ -10,7 +10,7 @@ var w_button_use_style ='background-color: blue;color: white;border: none;paddin
 function w_button_use_handler(widget_index)
 {
 	console.log("Button widget with Index: " + widget_index + " was pressed");
-	navigator.vibrate(200);
+	navigator.vibrate(50);
 	// execute the user defined action for this widget
 }
 
